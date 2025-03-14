0ddd06ea216ca3a1d70293d54f2ff7f12722358c
blogdopata <vcaballero_c@hotmail.com>
2025-03-14 17:19:20 -0500

    creando header

--- wp-content/themes/gymfitness/index.php
+++ wp-content/themes/gymfitness/index.php
@@ -1,37 +1,11 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gymfitness</title>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <header class="header">
-        <div class="contenedor  barra-navegacion">
-            
-            <div class="logo">
-                <img src="<?php echo get_template_directory_uri()?>/img/logo.svg" alt="Logo GymFItness">
-
-            </div>
-
-            <?php   
-                    // para indicar que menu quiero de los registrados en function .php
-                    //con el theme location le digo que menu quiero renderizar
-                $args = array(
-                    'theme_location' => 'menu-principal',
-                    'container' => 'nav',
-                    'container_class' => 'menu-principal'
-                );
-
-                wp_nav_menu($args);
-            ?>
+<?php
 
 
-        </div>
+    get_header();
+?>
 
 
-    </header>
+
 
     <main>
     <?php 
--- wp-content/themes/gymfitness/page.php
+++ wp-content/themes/gymfitness/page.php
@@ -1,48 +1,22 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gymfitness</title>
-    <?php wp_head(); ?>
-</head>
-<body>
-    
-    <header class="header">
-        <div class="contenedor  barra-navegacion">
+
+
+<?php
+    get_header();
+?>
+
+    <main class="contenedor seccion">
+        <?php 
+            while( have_posts() ): the_post();
+        ?>
+
+            <h1 class="text-center text-primary"> <?php the_title(); ?>   <h1>
             
-            <div class="logo">
-                <img src="<?php echo get_template_directory_uri()?>/img/logo.svg" alt="Logo GymFItness">
-
-            </div>
-
-            <?php   
-                    // para indicar que menu quiero de los registrados en function .php
-                    //con el theme location le digo que menu quiero renderizar
-                $args = array(
-                    'theme_location' => 'menu-principal',
-                    'container' => 'nav',
-                    'container_class' => 'menu-principal'
-                );
-
-                wp_nav_menu($args);
-            ?>
+        <?php
+                the_content();
+            endwhile;    
+        ?>
 
 
-        </div>
-
-
-    </header>
-
-    <main>
-    <?php 
-        while(have_posts()): the_post();
-
-        the_title();
-        the_content();
-
-        endwhile;    
-    ?>
     </main>
 </body>
 </html>
